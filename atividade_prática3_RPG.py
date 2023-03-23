1f90442029d75b0767f7fcee7b922d2ff73483d5
manupicoli <128271453+manupicoli@users.noreply.github.com>
2023-03-23 18:07:10 -0300

    Update atividade_prática3_RPG.py

--- atividade_prática3_RPG.py
+++ atividade_prática3_RPG.py
@@ -5,7 +5,7 @@ class Seres_Vivos:
         self.ponto_de_ataque = ponto_de_ataque
     
     def causar_dano(self, alvo): #o alvo vai ser o objeto que está sendo atacado
-        pontos_ganhos = self.ponto_de_ataque #os pontos ganhos com o ataque vão aumentar os pontos de ataque do atacante e diminuir os pontos de vida do atacado
+        pontos_ganhos = self.ponto_de_ataque #os pontos ganhos com o ataque vão diminuir os pontos de vida do atacado
         alvo.ponto_de_vida -= pontos_ganhos #diminui os pontos ganhos dos pontos de vida do alvo
         print(f'{self.__class__.__name__} causou {pontos_ganhos} pontos de dano em {alvo.__class__.__name__}!')
         if pontos_ganhos >= 30:
@@ -32,9 +32,9 @@ class Monstro(Seres_Vivos):
         self.tipo = tipo
 
 class Lobo(Monstro):
-    def __init__(self, ponto_de_vida, ponto_de_ataque, tipo, força):
+    def __init__(self, ponto_de_vida, ponto_de_ataque, tipo, forca):
         super().__init__(ponto_de_ataque, ponto_de_vida, tipo)
-        self.força = força
+        self.força = forca
 
 class Goblin(Monstro):
     def __init__(self, ponto_de_vida, ponto_de_ataque, tipo, inteligencia):
@@ -46,7 +46,3 @@ Lobinho = Lobo(50, 30, 'Lobo Alfa', 'Planetária') #está com 50 pts de vida e s
 Lobinho.causar_dano(goblinzinho)
 Lobinho.causar_dano(goblinzinho)
 Lobinho.causar_dano(goblinzinho)
-
-#atacar = recebe a quantidade de dano que pode causar e a quantidade de vida que o outro perdeu
-#ataque É o dano causado
-#qual ser vivo (personagem) vão receber esse dano
